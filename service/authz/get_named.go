f4699a716625fc61a86b5f55c67416ef0c2751c9
chrnorm <17420369+chrnorm@users.noreply.github.com>
2023-11-27 13:50:01 +0000

    update to GetNamed

--- service/authz/get_named.go
+++ service/authz/get_named.go
@@ -6,10 +6,10 @@ import (
 	accessv1alpha1 "github.com/common-fate/sdk/gen/commonfate/access/v1alpha1"
 )
 
-// GetName hydrates a resource with the 'name' attribute from the cache.
+// GetNamed hydrates a resource with the 'name' attribute from the cache.
 // This may result in additional API calls to the authz service if the entity is not
 // present in the client cache.
-func (c *Client) GetName(ctx context.Context, uid UID) *accessv1alpha1.NamedUID {
+func (c *Client) GetNamed(ctx context.Context, uid UID) *accessv1alpha1.NamedUID {
 	return &accessv1alpha1.NamedUID{
 		Uid:  uid.ToAPI(),
 		Name: c.AttrCache().String(ctx, uid, "name"),
