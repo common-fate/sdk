17b487a799ca15f68bf0c0f466e1ab60194c90a3
chrnorm <17420369+chrnorm@users.noreply.github.com>
2023-11-28 16:37:51 +0000

    add batchgetentities

---
+++ service/authz/batch_get_entities.go
@@ -0,0 +1,64 @@
+package authz
+
+import (
+	"context"
+
+	"github.com/bufbuild/connect-go"
+	authzv1alpha1 "github.com/common-fate/sdk/gen/commonfate/authz/v1alpha1"
+	"github.com/patrickmn/go-cache"
+)
+
+type BatchGetEntitiesInput struct {
+	UIDs []UID
+	// UseCache will try and retrieve entities from
+	// the client cache if it's present.
+	//
+	// If all entities are cached, no API call will be made.
+	UseCache bool
+}
+
+func (c *Client) BatchGetEntities(ctx context.Context, input BatchGetEntitiesInput) (*authzv1alpha1.BatchGetEntitiesResponse, error) {
+	req := &authzv1alpha1.BatchGetEntitiesRequest{
+		Universe: "default",
+	}
+
+	var cached []*authzv1alpha1.Entity
+
+	for _, u := range input.UIDs {
+		if input.UseCache {
+			if got, ok := c.cache.Get(u.String()); ok {
+				if entity, ok := got.(*authzv1alpha1.Entity); ok {
+					cached = append(cached, entity)
+					continue
+				}
+			}
+		}
+
+		req.Entities = append(req.Entities, u.ToAPI())
+	}
+
+	output := &authzv1alpha1.BatchGetEntitiesResponse{}
+
+	if len(req.Entities) > 0 {
+		res, err := c.raw.BatchGetEntities(ctx, connect.NewRequest(req))
+		if err != nil {
+			return nil, err
+		}
+		output = res.Msg
+
+		// update the attribute cache
+		for _, e := range res.Msg.Entities {
+			uid := UID{
+				Type: e.Uid.Type,
+				ID:   e.Uid.Id,
+			}
+
+			c.cache.Set(uid.String(), e, cache.DefaultExpiration)
+		}
+	}
+
+	// add any cached entities we retrieved
+	output.Entities = append(output.Entities, cached...)
+
+	return output, nil
+}
