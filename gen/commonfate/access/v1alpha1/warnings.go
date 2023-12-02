722e4b2ade44ed72a755f032f0d634b7b0f94c8d
chrnorm <17420369+chrnorm@users.noreply.github.com>
2023-12-02 16:31:59 +0000

    fix build

--- gen/commonfate/access/v1alpha1/warnings.go
+++
@@ -1,6 +0,0 @@
-package accessv1alpha1
-
-// OK returns true if there are no warnings.
-func (w *Warnings) OK() bool {
-	return len(w.GrantsInvalidStatus) == 0 && len(w.GrantsPermissionDenied) == 0
-}
