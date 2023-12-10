ad77e9d5d19c1cca47a5085b80c44139ab9c82c8
chrnorm <17420369+chrnorm@users.noreply.github.com>
2023-12-10 11:36:33 +0000

    add client creator for policy

--- service/authz/policyset/client.go
+++ service/authz/policyset/client.go
@@ -1,7 +1,26 @@
 package policyset
 
-import "github.com/common-fate/sdk/gen/commonfate/authz/v1alpha1/authzv1alpha1connect"
+import (
+	"github.com/bufbuild/connect-go"
+	"github.com/common-fate/sdk/gen/commonfate/authz/v1alpha1/authzv1alpha1connect"
+)
 
 type Client struct {
 	raw authzv1alpha1connect.PolicyServiceClient
+}
+
+type Opts struct {
+	HTTPClient    connect.HTTPClient
+	BaseURL       string
+	ClientOptions []connect.ClientOption
+}
+
+func NewClient(opts Opts) Client {
+	// client uses GRPC by default as the authz service does not support Buf Connect.
+	connectOpts := []connect.ClientOption{connect.WithGRPC()}
+	connectOpts = append(connectOpts, opts.ClientOptions...)
+
+	rawClient := authzv1alpha1connect.NewPolicyServiceClient(opts.HTTPClient, opts.BaseURL, connectOpts...)
+
+	return Client{raw: rawClient}
 }
