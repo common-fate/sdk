1f973a5eb935f01d870876500eeb28d2c21cad85
chrnorm <17420369+chrnorm@users.noreply.github.com>
2023-12-10 11:23:19 +0000

    fix + add delete

---
+++ service/authz/policyset/delete.go
@@ -0,0 +1,31 @@
+package policyset
+
+import (
+	"context"
+
+	"github.com/bufbuild/connect-go"
+	authzv1alpha1 "github.com/common-fate/sdk/gen/commonfate/authz/v1alpha1"
+)
+
+type DeleteInput struct {
+	ID string
+}
+
+type DeleteOutput struct {
+	ID string
+}
+
+func (c *Client) Delete(ctx context.Context, input DeleteInput) (DeleteOutput, error) {
+	apires, err := c.raw.DeletePolicySet(ctx, connect.NewRequest(&authzv1alpha1.DeletePolicySetRequest{
+		Id: input.ID,
+	}))
+	if err != nil {
+		return DeleteOutput{}, err
+	}
+
+	res := DeleteOutput{
+		ID: apires.Msg.Id,
+	}
+
+	return res, nil
+}
