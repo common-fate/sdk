fea533aec1b4ea02d075d782b792c3e2f61df20a
Josh Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2024-01-16 16:14:31 +0800

    revert provisioner context

--- provisioner/provisioner.go
+++ provisioner/provisioner.go
@@ -5,21 +5,12 @@ import (
 	"github.com/common-fate/sdk/service/entity"
 )
 
-type Context struct {
-	Attr1 string `json:"attr_1"`
-	Attr2 string `json:"attr_2"`
-	Attr3 string `json:"attr_3"`
-	Attr4 string `json:"attr_4"`
-	Attr5 string `json:"attr_5"`
-}
-
 type Grant struct {
-	Grant              eid.EID  `json:"grant"`
-	Principal          eid.EID  `json:"principal"`
-	MappedPrincipal    *eid.EID `json:"mapped_principal,omitempty"`
-	Target             eid.EID  `json:"target"`
-	Role               eid.EID  `json:"role"`
-	ProvisionerContext Context  `json:"provisioner_context"`
+	Grant           eid.EID  `json:"grant"`
+	Principal       eid.EID  `json:"principal"`
+	MappedPrincipal *eid.EID `json:"mapped_principal,omitempty"`
+	Target          eid.EID  `json:"target"`
+	Role            eid.EID  `json:"role"`
 }
 
 type GrantResponse struct {
