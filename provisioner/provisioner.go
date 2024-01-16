7d88c2c79ce03b46d513ee12665ad614899ea189
Josh Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2024-01-16 16:01:52 +0800

    use basic context struct

--- provisioner/provisioner.go
+++ provisioner/provisioner.go
@@ -6,17 +6,20 @@ import (
 )
 
 type Context struct {
-	Key   string `json:"key"`
-	Value string `json:"value"`
+	Attr1 string `json:"attr_1"`
+	Attr2 string `json:"attr_2"`
+	Attr3 string `json:"attr_3"`
+	Attr4 string `json:"attr_4"`
+	Attr5 string `json:"attr_5"`
 }
 
 type Grant struct {
-	Grant              eid.EID   `json:"grant"`
-	Principal          eid.EID   `json:"principal"`
-	MappedPrincipal    *eid.EID  `json:"mapped_principal,omitempty"`
-	Target             eid.EID   `json:"target"`
-	Role               eid.EID   `json:"role"`
-	ProvisionerContext []Context `json:"provisioner_context"`
+	Grant              eid.EID  `json:"grant"`
+	Principal          eid.EID  `json:"principal"`
+	MappedPrincipal    *eid.EID `json:"mapped_principal,omitempty"`
+	Target             eid.EID  `json:"target"`
+	Role               eid.EID  `json:"role"`
+	ProvisionerContext Context  `json:"provisioner_context"`
 }
 
 type GrantResponse struct {
