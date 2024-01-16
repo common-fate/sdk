2e4eba6956c3f708d27d2c518bfc85ab9ad2174e
Josh Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2024-01-16 15:56:35 +0800

    use context instead

--- provisioner/provisioner.go
+++ provisioner/provisioner.go
@@ -5,13 +5,18 @@ import (
 	"github.com/common-fate/sdk/service/entity"
 )
 
+type Context struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
 type Grant struct {
-	Grant              eid.EID           `json:"grant"`
-	Principal          eid.EID           `json:"principal"`
-	MappedPrincipal    *eid.EID          `json:"mapped_principal,omitempty"`
-	Target             eid.EID           `json:"target"`
-	Role               eid.EID           `json:"role"`
-	ProvisionerContext map[string]string `json:"provisioner_context"`
+	Grant              eid.EID   `json:"grant"`
+	Principal          eid.EID   `json:"principal"`
+	MappedPrincipal    *eid.EID  `json:"mapped_principal,omitempty"`
+	Target             eid.EID   `json:"target"`
+	Role               eid.EID   `json:"role"`
+	ProvisionerContext []Context `json:"provisioner_context"`
 }
 
 type GrantResponse struct {
