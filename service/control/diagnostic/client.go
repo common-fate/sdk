ec2e18a369c0e41f288949be7f89e648abe8c07f
chrnorm <17420369+chrnorm@users.noreply.github.com>
2024-03-05 14:07:51 +0000

    update package to commonfate.control.diagnostic.v1alpha1
    (avoids needing to reconfigure ingress rules in ALB)

--- service/control/diagnostic/client.go
+++ service/control/diagnostic/client.go
@@ -2,7 +2,7 @@ package diagnostic
 
 import (
 	"github.com/common-fate/sdk/config"
-	"github.com/common-fate/sdk/gen/commonfate/deployment/diagnostic/v1alpha1/diagnosticv1alpha1connect"
+	"github.com/common-fate/sdk/gen/commonfate/control/diagnostic/v1alpha1/diagnosticv1alpha1connect"
 )
 
 func NewFromConfig(cfg *config.Context) diagnosticv1alpha1connect.DiagnosticServiceClient {
