7559ba091056700ba304bfe55e2fa4c0d1e3d1c7
chrnorm <17420369+chrnorm@users.noreply.github.com>
2024-03-20 14:28:20 +0000

    add tag support and remove client key support from authz API
    client keys are currently unused in our internal application code

--- service/authz/batch_authorize.go
+++ service/authz/batch_authorize.go
@@ -21,32 +21,15 @@ type BatchAuthorizeInput struct {
 }
 
 type BatchAuthorizeResponse struct {
-	// Decisions in the response.
-	// Usually you'll want GetDecision() to look up a specific decision
-	// which checks it exists.
-	Decisions map[string]*authzv1alpha1.Evaluation
-}
-
-// GetDecision looks up a decision by the client key. The client key is the key to the map
-// that you provided in BatchAuthorizeInput.
-func (r BatchAuthorizeResponse) GetDecision(key string) (*authzv1alpha1.Evaluation, error) {
-	got, ok := r.Decisions[key]
-	if !ok {
-		return nil, DecisionNotFoundError{Key: key}
-	}
-
-	if got == nil {
-		return nil, fmt.Errorf("decision %s was nil", key)
-	}
-
-	return got, nil
+	// Evaluations made by the authz service.
+	Evaluations []*authzv1alpha1.Evaluation
 }
 
 func (c *Client) BatchAuthorize(ctx context.Context, input BatchAuthorizeInput) (BatchAuthorizeResponse, error) {
 	r := make([]*authzv1alpha1.Request, 0)
 
-	for k, item := range input.Requests {
-		req, err := item.ToAPI(k)
+	for _, item := range input.Requests {
+		req, err := item.ToAPI()
 		if err != nil {
 			return BatchAuthorizeResponse{}, err
 		}
@@ -61,11 +44,7 @@ func (c *Client) BatchAuthorize(ctx context.Context, input BatchAuthorizeInput) 
 	}
 
 	ret := BatchAuthorizeResponse{
-		Decisions: map[string]*authzv1alpha1.Evaluation{},
-	}
-
-	for _, d := range res.Msg.Evaluations {
-		ret.Decisions[d.ClientKey] = d
+		Evaluations: res.Msg.Evaluations,
 	}
 
 	return ret, nil
