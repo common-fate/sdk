5756a5f2832840532accd0acd0cd70457c8bc65d
chrnorm <17420369+chrnorm@users.noreply.github.com>
2024-04-16 09:02:48 +0100

    add cloudsupport client

---
+++ factoryconfig/config.go
@@ -0,0 +1,49 @@
+// Package factoryconfig holds configuration for the Factory service SDK.
+// This service is hosted by Common Fate, so configuring a client is slightly
+// different than configuring a client for a Common Fate deployment.
+package factoryconfig
+
+import (
+	"context"
+	"net/http"
+)
+
+type Opts struct {
+	// BaseURL of the Factory service to connect to.
+	// Defaults to "https://factory.commonfate.io"
+	// if not provided.
+	BaseURL string
+	// A custom HTTP Client. Defaults to http.DefaultClient
+	// if not provided.
+	HTTPClient *http.Client
+}
+
+type Context struct {
+	// BaseURL of the Factory service to connect to.
+	BaseURL string
+	// The HTTP client to use when making requests.
+	HTTPClient *http.Client
+}
+
+// LoadDefault loads a client context with all defaults configured.
+func LoadDefault(ctx context.Context) (*Context, error) {
+	return Load(ctx, Opts{})
+}
+
+// Load a client context. You can override values by providing them in 'opts'.
+func Load(ctx context.Context, opts Opts) (*Context, error) {
+	if opts.BaseURL == "" {
+		opts.BaseURL = "https://factory.commonfate.io"
+	}
+
+	if opts.HTTPClient == nil {
+		opts.HTTPClient = http.DefaultClient
+	}
+
+	c := Context{
+		BaseURL:    opts.BaseURL,
+		HTTPClient: opts.HTTPClient,
+	}
+
+	return &c, nil
+}
