1f9474a620ee3647f562053a9364373c37357ba6
chrnorm <17420369+chrnorm@users.noreply.github.com>
2024-07-09 14:23:56 +0100

    update spec

--- gen/commonfate/factory/usage/v1alpha1/usage.pb.validate.go
+++ gen/commonfate/factory/usage/v1alpha1/usage.pb.validate.go
@@ -182,40 +182,6 @@ func (m *ReportUsageRequest) validate(all bool) error {
 		}
 	}
 
-	for idx, item := range m.GetEntitlements() {
-		_, _ = idx, item
-
-		if all {
-			switch v := interface{}(item).(type) {
-			case interface{ ValidateAll() error }:
-				if err := v.ValidateAll(); err != nil {
-					errors = append(errors, ReportUsageRequestValidationError{
-						field:  fmt.Sprintf("Entitlements[%v]", idx),
-						reason: "embedded message failed validation",
-						cause:  err,
-					})
-				}
-			case interface{ Validate() error }:
-				if err := v.Validate(); err != nil {
-					errors = append(errors, ReportUsageRequestValidationError{
-						field:  fmt.Sprintf("Entitlements[%v]", idx),
-						reason: "embedded message failed validation",
-						cause:  err,
-					})
-				}
-			}
-		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
-			if err := v.Validate(); err != nil {
-				return ReportUsageRequestValidationError{
-					field:  fmt.Sprintf("Entitlements[%v]", idx),
-					reason: "embedded message failed validation",
-					cause:  err,
-				}
-			}
-		}
-
-	}
-
 	if len(errors) > 0 {
 		return ReportUsageRequestMultiError(errors)
 	}
@@ -713,6 +679,8 @@ func (m *IntegrationUsage) validate(all bool) error {
 
 	// no validation rules for GroupCount
 
+	// no validation rules for TargetCount
+
 	if len(errors) > 0 {
 		return IntegrationUsageMultiError(errors)
 	}
@@ -790,110 +758,6 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = IntegrationUsageValidationError{}
-
-// Validate checks the field values on EntitlementUsage with the rules defined
-// in the proto definition for this message. If any rules are violated, the
-// first error encountered is returned, or nil if there are no violations.
-func (m *EntitlementUsage) Validate() error {
-	return m.validate(false)
-}
-
-// ValidateAll checks the field values on EntitlementUsage with the rules
-// defined in the proto definition for this message. If any rules are
-// violated, the result is a list of violation errors wrapped in
-// EntitlementUsageMultiError, or nil if none found.
-func (m *EntitlementUsage) ValidateAll() error {
-	return m.validate(true)
-}
-
-func (m *EntitlementUsage) validate(all bool) error {
-	if m == nil {
-		return nil
-	}
-
-	var errors []error
-
-	// no validation rules for TargetType
-
-	// no validation rules for TargetCount
-
-	if len(errors) > 0 {
-		return EntitlementUsageMultiError(errors)
-	}
-
-	return nil
-}
-
-// EntitlementUsageMultiError is an error wrapping multiple validation errors
-// returned by EntitlementUsage.ValidateAll() if the designated constraints
-// aren't met.
-type EntitlementUsageMultiError []error
-
-// Error returns a concatenation of all the error messages it wraps.
-func (m EntitlementUsageMultiError) Error() string {
-	var msgs []string
-	for _, err := range m {
-		msgs = append(msgs, err.Error())
-	}
-	return strings.Join(msgs, "; ")
-}
-
-// AllErrors returns a list of validation violation errors.
-func (m EntitlementUsageMultiError) AllErrors() []error { return m }
-
-// EntitlementUsageValidationError is the validation error returned by
-// EntitlementUsage.Validate if the designated constraints aren't met.
-type EntitlementUsageValidationError struct {
-	field  string
-	reason string
-	cause  error
-	key    bool
-}
-
-// Field function returns field value.
-func (e EntitlementUsageValidationError) Field() string { return e.field }
-
-// Reason function returns reason value.
-func (e EntitlementUsageValidationError) Reason() string { return e.reason }
-
-// Cause function returns cause value.
-func (e EntitlementUsageValidationError) Cause() error { return e.cause }
-
-// Key function returns key value.
-func (e EntitlementUsageValidationError) Key() bool { return e.key }
-
-// ErrorName returns error name.
-func (e EntitlementUsageValidationError) ErrorName() string { return "EntitlementUsageValidationError" }
-
-// Error satisfies the builtin error interface
-func (e EntitlementUsageValidationError) Error() string {
-	cause := ""
-	if e.cause != nil {
-		cause = fmt.Sprintf(" | caused by: %v", e.cause)
-	}
-
-	key := ""
-	if e.key {
-		key = "key for "
-	}
-
-	return fmt.Sprintf(
-		"invalid %sEntitlementUsage.%s: %s%s",
-		key,
-		e.field,
-		e.reason,
-		cause)
-}
-
-var _ error = EntitlementUsageValidationError{}
-
-var _ interface {
-	Field() string
-	Reason() string
-	Key() bool
-	Cause() error
-	ErrorName() string
-} = EntitlementUsageValidationError{}
 
 // Validate checks the field values on Distribution with the rules defined in
 // the proto definition for this message. If any rules are violated, the first
