6218bbbbcf96584c259cd7f8863170f172c07bf3
chrnorm <17420369+chrnorm@users.noreply.github.com>
2024-07-22 12:15:55 +0100

    Factory service: allow client opts to be provided
    Internal use case is to enable OpenTelemetry tracing for the RPC client libraries.
    If rollout works here we can update all clients.

--- factory/service/monitoring/client.go
+++ factory/service/monitoring/client.go
@@ -1,22 +1,24 @@
 package monitoring
 
 import (
+	"connectrpc.com/connect"
 	"github.com/common-fate/sdk/factoryconfig"
 	"github.com/common-fate/sdk/gen/commonfate/factory/monitoring/v1alpha1/monitoringv1alpha1connect"
 )
 
 type Client struct {
-	cfg *factoryconfig.Context
+	cfg  *factoryconfig.Context
+	opts []connect.ClientOption
 }
 
-func NewFromConfig(cfg *factoryconfig.Context) *Client {
-	return &Client{cfg: cfg}
+func NewFromConfig(cfg *factoryconfig.Context, opts ...connect.ClientOption) *Client {
+	return &Client{cfg: cfg, opts: opts}
 }
 
 func (c *Client) Tokens() monitoringv1alpha1connect.TokenServiceClient {
-	return monitoringv1alpha1connect.NewTokenServiceClient(c.cfg.HTTPClient, c.cfg.BaseURL)
+	return monitoringv1alpha1connect.NewTokenServiceClient(c.cfg.HTTPClient, c.cfg.BaseURL, c.opts...)
 }
 
 func (c *Client) Validation() monitoringv1alpha1connect.ValidationServiceClient {
-	return monitoringv1alpha1connect.NewValidationServiceClient(c.cfg.HTTPClient, c.cfg.BaseURL)
+	return monitoringv1alpha1connect.NewValidationServiceClient(c.cfg.HTTPClient, c.cfg.BaseURL, c.opts...)
 }
