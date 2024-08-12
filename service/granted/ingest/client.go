4880cf226a5eddd1f4abfc58695d996167c0fb5f
chrnorm <17420369+chrnorm@users.noreply.github.com>
2024-08-12 16:28:11 +0100

    update spec

--- service/granted/ingest/client.go
+++ service/granted/ingest/client.go
@@ -2,9 +2,9 @@ package ingest
 
 import (
 	"github.com/common-fate/sdk/config"
-	"github.com/common-fate/sdk/gen/granted/ingest/aws/v1alpha1/awsv1alpha1connect"
+	"github.com/common-fate/sdk/gen/granted/ingest/v1alpha1/ingestv1alpha1connect"
 )
 
-func NewFromConfig(cfg *config.Context) awsv1alpha1connect.IngestServiceClient {
-	return awsv1alpha1connect.NewIngestServiceClient(cfg.HTTPClient, cfg.APIURL)
+func NewFromConfig(cfg *config.Context) ingestv1alpha1connect.IngestServiceClient {
+	return ingestv1alpha1connect.NewIngestServiceClient(cfg.HTTPClient, cfg.APIURL)
 }
