35f2f5f025847e91898166c435f1425acfdeec06
meyerjrr <jrmeyer339@gmail.com>
2024-09-09 16:47:39 +1000

    added to request

--- gen/commonfate/control/integration/v1alpha1/integration.pb.validate.go
+++ gen/commonfate/control/integration/v1alpha1/integration.pb.validate.go
@@ -2181,6 +2181,35 @@ func (m *ListSyncsForIntegrationRequest) validate(all bool) error {
 
 	// no validation rules for PageToken
 
+	if all {
+		switch v := interface{}(m.GetFilder()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, ListSyncsForIntegrationRequestValidationError{
+					field:  "Filder",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, ListSyncsForIntegrationRequestValidationError{
+					field:  "Filder",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetFilder()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return ListSyncsForIntegrationRequestValidationError{
+				field:  "Filder",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return ListSyncsForIntegrationRequestMultiError(errors)
 	}
