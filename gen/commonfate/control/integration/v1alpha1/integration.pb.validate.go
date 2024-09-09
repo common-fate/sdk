652c28a98f2290604bcddb2813ce0035b2b898cd
meyerjrr <jrmeyer339@gmail.com>
2024-09-09 17:44:50 +1000

    added diagnostics to syncintegration response

--- gen/commonfate/control/integration/v1alpha1/integration.pb.validate.go
+++ gen/commonfate/control/integration/v1alpha1/integration.pb.validate.go
@@ -2702,6 +2702,40 @@ func (m *SyncIntegrationResponse) validate(all bool) error {
 
 	var errors []error
 
+	for idx, item := range m.GetDiagnostics() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, SyncIntegrationResponseValidationError{
+						field:  fmt.Sprintf("Diagnostics[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, SyncIntegrationResponseValidationError{
+						field:  fmt.Sprintf("Diagnostics[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return SyncIntegrationResponseValidationError{
+					field:  fmt.Sprintf("Diagnostics[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
 	if len(errors) > 0 {
 		return SyncIntegrationResponseMultiError(errors)
 	}
