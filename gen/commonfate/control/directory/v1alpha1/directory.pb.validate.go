e14da90ff2b7b965297e6cd19776030b12731b47
meyerjrr <jrmeyer339@gmail.com>
2024-09-17 16:33:04 +1000

    remove diagnostics to delete response

--- gen/commonfate/control/directory/v1alpha1/directory.pb.validate.go
+++ gen/commonfate/control/directory/v1alpha1/directory.pb.validate.go
@@ -405,40 +405,6 @@ func (m *DeleteUserResponse) validate(all bool) error {
 
 	var errors []error
 
-	for idx, item := range m.GetDiagnostics() {
-		_, _ = idx, item
-
-		if all {
-			switch v := interface{}(item).(type) {
-			case interface{ ValidateAll() error }:
-				if err := v.ValidateAll(); err != nil {
-					errors = append(errors, DeleteUserResponseValidationError{
-						field:  fmt.Sprintf("Diagnostics[%v]", idx),
-						reason: "embedded message failed validation",
-						cause:  err,
-					})
-				}
-			case interface{ Validate() error }:
-				if err := v.Validate(); err != nil {
-					errors = append(errors, DeleteUserResponseValidationError{
-						field:  fmt.Sprintf("Diagnostics[%v]", idx),
-						reason: "embedded message failed validation",
-						cause:  err,
-					})
-				}
-			}
-		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
-			if err := v.Validate(); err != nil {
-				return DeleteUserResponseValidationError{
-					field:  fmt.Sprintf("Diagnostics[%v]", idx),
-					reason: "embedded message failed validation",
-					cause:  err,
-				}
-			}
-		}
-
-	}
-
 	if len(errors) > 0 {
 		return DeleteUserResponseMultiError(errors)
 	}
