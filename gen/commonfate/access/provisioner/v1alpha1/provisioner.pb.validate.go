d00ce9e962216800afc1840d32095aa86baad820
meyerjrr <jrmeyer339@gmail.com>
2024-09-23 14:30:28 +1000

    updated to be additional configs for aws idc integrations

--- gen/commonfate/access/provisioner/v1alpha1/provisioner.pb.validate.go
+++ gen/commonfate/access/provisioner/v1alpha1/provisioner.pb.validate.go
@@ -260,8 +260,38 @@ func (m *ProvisionRequest) validate(all bool) error {
 		}
 	}
 
-	if m.CustomSsoStartUrl != nil {
-		// no validation rules for CustomSsoStartUrl
+	for idx, item := range m.GetAdditionalIntegrations() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, ProvisionRequestValidationError{
+						field:  fmt.Sprintf("AdditionalIntegrations[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, ProvisionRequestValidationError{
+						field:  fmt.Sprintf("AdditionalIntegrations[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return ProvisionRequestValidationError{
+					field:  fmt.Sprintf("AdditionalIntegrations[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
 	}
 
 	if len(errors) > 0 {
