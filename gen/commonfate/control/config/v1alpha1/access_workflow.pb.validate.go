6d428a7730882d95125938b1a3565b7634f9ba17
ckluy31 <calvinluy@gmail.com>
2024-09-25 10:28:32 +1000

    Add RegexValidation to ValidationConfig in access workflow

--- gen/commonfate/control/config/v1alpha1/access_workflow.pb.validate.go
+++ gen/commonfate/control/config/v1alpha1/access_workflow.pb.validate.go
@@ -339,6 +339,40 @@ func (m *ValidationConfig) validate(all bool) error {
 
 	// no validation rules for HasReason
 
+	for idx, item := range m.GetRegex() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, ValidationConfigValidationError{
+						field:  fmt.Sprintf("Regex[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, ValidationConfigValidationError{
+						field:  fmt.Sprintf("Regex[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return ValidationConfigValidationError{
+					field:  fmt.Sprintf("Regex[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
 	if len(errors) > 0 {
 		return ValidationConfigMultiError(errors)
 	}
