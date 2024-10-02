ff9f5c7c614b74d271ba9a202e8c2d329de39369
JoshuaWilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2024-10-02 11:14:12 +0800

    generate

--- gen/commonfate/control/integration/v1alpha1/aws_proxy.pb.validate.go
+++ gen/commonfate/control/integration/v1alpha1/aws_proxy.pb.validate.go
@@ -63,6 +63,8 @@ func (m *AWSProxy) validate(all bool) error {
 
 	// no validation rules for IdcRegion
 
+	// no validation rules for AwsIntegrationId
+
 	switch v := m.InstanceConfig.(type) {
 	case *AWSProxy_AwsEcsProxyInstanceConfig:
 		if v == nil {
