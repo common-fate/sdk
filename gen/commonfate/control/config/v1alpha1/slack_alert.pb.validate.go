b4f25d3ea490078238e7f72eca32d9aa79fd2583
meyerjrr <jrmeyer339@gmail.com>
2024-10-08 13:50:11 +1100

    update field name

--- gen/commonfate/control/config/v1alpha1/slack_alert.pb.validate.go
+++ gen/commonfate/control/config/v1alpha1/slack_alert.pb.validate.go
@@ -98,7 +98,7 @@ func (m *CreateSlackAlertRequest) validate(all bool) error {
 		}
 	}
 
-	// no validation rules for DisableForAutoApprovedRequests
+	// no validation rules for DisableChannelMessageForAutoapprovedRequests
 
 	if m.IntegrationId != nil {
 		// no validation rules for IntegrationId
@@ -249,7 +249,7 @@ func (m *SlackAlert) validate(all bool) error {
 		}
 	}
 
-	// no validation rules for DisableForAutoApprovedRequests
+	// no validation rules for DisableChannelMessageForAutoapprovedRequests
 
 	if m.IntegrationId != nil {
 		// no validation rules for IntegrationId
