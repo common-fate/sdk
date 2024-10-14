9afcd6a63fb5d8c0a619bb729c1a758cb37183f7
ckluy31 <calvinluy@gmail.com>
2024-10-14 14:05:33 +1100

    remove diagnostics

--- gen/commonfate/access/v1alpha1/attachments.pb.validate.go
+++ gen/commonfate/access/v1alpha1/attachments.pb.validate.go
@@ -541,40 +541,6 @@ func (m *AttachRequestCommentToJiraIssuesResponse) validate(all bool) error {
 
 	var errors []error
 
-	for idx, item := range m.GetDiagnostics() {
-		_, _ = idx, item
-
-		if all {
-			switch v := interface{}(item).(type) {
-			case interface{ ValidateAll() error }:
-				if err := v.ValidateAll(); err != nil {
-					errors = append(errors, AttachRequestCommentToJiraIssuesResponseValidationError{
-						field:  fmt.Sprintf("Diagnostics[%v]", idx),
-						reason: "embedded message failed validation",
-						cause:  err,
-					})
-				}
-			case interface{ Validate() error }:
-				if err := v.Validate(); err != nil {
-					errors = append(errors, AttachRequestCommentToJiraIssuesResponseValidationError{
-						field:  fmt.Sprintf("Diagnostics[%v]", idx),
-						reason: "embedded message failed validation",
-						cause:  err,
-					})
-				}
-			}
-		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
-			if err := v.Validate(); err != nil {
-				return AttachRequestCommentToJiraIssuesResponseValidationError{
-					field:  fmt.Sprintf("Diagnostics[%v]", idx),
-					reason: "embedded message failed validation",
-					cause:  err,
-				}
-			}
-		}
-
-	}
-
 	if len(errors) > 0 {
 		return AttachRequestCommentToJiraIssuesResponseMultiError(errors)
 	}
