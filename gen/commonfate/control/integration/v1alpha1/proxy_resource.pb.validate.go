1a191a8306333041b3ed8d144b5af16fbd262e8f
JoshuaWilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2024-10-17 09:10:51 +0800

    Rework EKS resources

--- gen/commonfate/control/integration/v1alpha1/proxy_resource.pb.validate.go
+++ gen/commonfate/control/integration/v1alpha1/proxy_resource.pb.validate.go
@@ -509,6 +509,46 @@ func (m *AWSEKSCluster) validate(all bool) error {
 
 	// no validation rules for Account
 
+	// no validation rules for ClusterName
+
+	for idx, item := range m.GetUsers() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, AWSEKSClusterValidationError{
+						field:  fmt.Sprintf("Users[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, AWSEKSClusterValidationError{
+						field:  fmt.Sprintf("Users[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return AWSEKSClusterValidationError{
+					field:  fmt.Sprintf("Users[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
+	if m.ClusterAccessRoleName != nil {
+		// no validation rules for ClusterAccessRoleName
+	}
+
 	if len(errors) > 0 {
 		return AWSEKSClusterMultiError(errors)
 	}
@@ -586,3 +626,109 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = AWSEKSClusterValidationError{}
+
+// Validate checks the field values on AWSEKSClusterUser with the rules defined
+// in the proto definition for this message. If any rules are violated, the
+// first error encountered is returned, or nil if there are no violations.
+func (m *AWSEKSClusterUser) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on AWSEKSClusterUser with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// AWSEKSClusterUserMultiError, or nil if none found.
+func (m *AWSEKSClusterUser) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *AWSEKSClusterUser) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for Name
+
+	// no validation rules for ServiceAccountName
+
+	if len(errors) > 0 {
+		return AWSEKSClusterUserMultiError(errors)
+	}
+
+	return nil
+}
+
+// AWSEKSClusterUserMultiError is an error wrapping multiple validation errors
+// returned by AWSEKSClusterUser.ValidateAll() if the designated constraints
+// aren't met.
+type AWSEKSClusterUserMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m AWSEKSClusterUserMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m AWSEKSClusterUserMultiError) AllErrors() []error { return m }
+
+// AWSEKSClusterUserValidationError is the validation error returned by
+// AWSEKSClusterUser.Validate if the designated constraints aren't met.
+type AWSEKSClusterUserValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e AWSEKSClusterUserValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e AWSEKSClusterUserValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e AWSEKSClusterUserValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e AWSEKSClusterUserValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e AWSEKSClusterUserValidationError) ErrorName() string {
+	return "AWSEKSClusterUserValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e AWSEKSClusterUserValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sAWSEKSClusterUser.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = AWSEKSClusterUserValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = AWSEKSClusterUserValidationError{}
