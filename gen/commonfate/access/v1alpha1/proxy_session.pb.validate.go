6902561be8e1a52c198e96ace21ea8a42b92d28d
Calvin <70191007+ckluy31@users.noreply.github.com>
2024-10-18 16:14:35 +1100

    Add strongly typed representation of the Kubernetes Action (#283)
    * Add strongly typed representation of the Kubernetes Action
    
    * remove KubernetesExecMetadata

--- gen/commonfate/access/v1alpha1/proxy_session.pb.validate.go
+++ gen/commonfate/access/v1alpha1/proxy_session.pb.validate.go
@@ -506,6 +506,52 @@ func (m *PutSessionLogRequest) validate(all bool) error {
 		}
 	}
 
+	switch v := m.Detail.(type) {
+	case *PutSessionLogRequest_KubernetesAction:
+		if v == nil {
+			err := PutSessionLogRequestValidationError{
+				field:  "Detail",
+				reason: "oneof value cannot be a typed-nil",
+			}
+			if !all {
+				return err
+			}
+			errors = append(errors, err)
+		}
+
+		if all {
+			switch v := interface{}(m.GetKubernetesAction()).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, PutSessionLogRequestValidationError{
+						field:  "KubernetesAction",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, PutSessionLogRequestValidationError{
+						field:  "KubernetesAction",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(m.GetKubernetesAction()).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return PutSessionLogRequestValidationError{
+					field:  "KubernetesAction",
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	default:
+		_ = v // ensures v is used
+	}
+
 	if len(errors) > 0 {
 		return PutSessionLogRequestMultiError(errors)
 	}
@@ -585,6 +631,132 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = PutSessionLogRequestValidationError{}
+
+// Validate checks the field values on KubernetesAction with the rules defined
+// in the proto definition for this message. If any rules are violated, the
+// first error encountered is returned, or nil if there are no violations.
+func (m *KubernetesAction) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on KubernetesAction with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// KubernetesActionMultiError, or nil if none found.
+func (m *KubernetesAction) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *KubernetesAction) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for ActionName
+
+	// no validation rules for Cluster
+
+	// no validation rules for HttpMethod
+
+	// no validation rules for ImpersonateUserHeader
+
+	// no validation rules for RequestUri
+
+	// no validation rules for Role
+
+	// no validation rules for StatusCode
+
+	if m.Error != nil {
+		// no validation rules for Error
+	}
+
+	if m.Namespace != nil {
+		// no validation rules for Namespace
+	}
+
+	if m.Pod != nil {
+		// no validation rules for Pod
+	}
+
+	if len(errors) > 0 {
+		return KubernetesActionMultiError(errors)
+	}
+
+	return nil
+}
+
+// KubernetesActionMultiError is an error wrapping multiple validation errors
+// returned by KubernetesAction.ValidateAll() if the designated constraints
+// aren't met.
+type KubernetesActionMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m KubernetesActionMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m KubernetesActionMultiError) AllErrors() []error { return m }
+
+// KubernetesActionValidationError is the validation error returned by
+// KubernetesAction.Validate if the designated constraints aren't met.
+type KubernetesActionValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e KubernetesActionValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e KubernetesActionValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e KubernetesActionValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e KubernetesActionValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e KubernetesActionValidationError) ErrorName() string { return "KubernetesActionValidationError" }
+
+// Error satisfies the builtin error interface
+func (e KubernetesActionValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sKubernetesAction.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = KubernetesActionValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = KubernetesActionValidationError{}
 
 // Validate checks the field values on PutSessionLogResponse with the rules
 // defined in the proto definition for this message. If any rules are
