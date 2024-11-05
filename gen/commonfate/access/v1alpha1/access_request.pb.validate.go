142ecc771e30e960d2a9bfbf58e4c8dfc5e4d3cf
meyerjrr <jrmeyer339@gmail.com>
2024-11-05 16:52:37 +1100

    added filter for occured at

--- gen/commonfate/access/v1alpha1/access_request.pb.validate.go
+++ gen/commonfate/access/v1alpha1/access_request.pb.validate.go
@@ -471,6 +471,47 @@ func (m *Filter) validate(all bool) error {
 			}
 		}
 
+	case *Filter_OccurredAt:
+		if v == nil {
+			err := FilterValidationError{
+				field:  "Filter",
+				reason: "oneof value cannot be a typed-nil",
+			}
+			if !all {
+				return err
+			}
+			errors = append(errors, err)
+		}
+
+		if all {
+			switch v := interface{}(m.GetOccurredAt()).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, FilterValidationError{
+						field:  "OccurredAt",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, FilterValidationError{
+						field:  "OccurredAt",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(m.GetOccurredAt()).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return FilterValidationError{
+					field:  "OccurredAt",
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
 	default:
 		_ = v // ensures v is used
 	}
