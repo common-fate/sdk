ab1913f1ed1f38c84d8f0cfd868631ffdfd33663
meyerjrr <jrmeyer339@gmail.com>
2024-11-13 10:32:01 +1100

    added ended at filter

--- gen/commonfate/access/v1alpha1/proxy_session.pb.validate.go
+++ gen/commonfate/access/v1alpha1/proxy_session.pb.validate.go
@@ -1002,6 +1002,47 @@ func (m *SessionLogFilter) validate(all bool) error {
 			}
 		}
 
+	case *SessionLogFilter_EndedAt:
+		if v == nil {
+			err := SessionLogFilterValidationError{
+				field:  "Filter",
+				reason: "oneof value cannot be a typed-nil",
+			}
+			if !all {
+				return err
+			}
+			errors = append(errors, err)
+		}
+
+		if all {
+			switch v := interface{}(m.GetEndedAt()).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, SessionLogFilterValidationError{
+						field:  "EndedAt",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, SessionLogFilterValidationError{
+						field:  "EndedAt",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(m.GetEndedAt()).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return SessionLogFilterValidationError{
+					field:  "EndedAt",
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
 	default:
 		_ = v // ensures v is used
 	}
